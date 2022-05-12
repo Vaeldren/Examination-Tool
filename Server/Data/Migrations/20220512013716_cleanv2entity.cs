aaee3fdfd60efd8e1134ede58674aa4a4b172f94
Abraham Aidid <w1728169@westminster.ac.uk>
2022-05-12 02:41:48 +0100

    i kind of get what im doing now

---
+++ Server/Data/Migrations/20220512013716_cleanv2entity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalYearProject.Server.Data.Migrations
+{
+    public partial class cleanv2entity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
